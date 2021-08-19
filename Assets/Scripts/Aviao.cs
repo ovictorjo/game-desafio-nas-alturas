ccea1b0f1be6218a11444a1f29c02b2fcee2f3a3
Joao Victor <ovictorjocorvinal@gmail.com>
2021-08-19 09:47:44 -0300

    Adicionando impulso ao avião

---
+++ Assets/Scripts/Aviao.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+    private Rigidbody2D fisica;
+
+    private void Awake()
+    {
+        this.fisica = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Impulsionar();
+        }
+    }
+
+    private void Impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+    
+}
