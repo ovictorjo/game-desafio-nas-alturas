60c4b245abb5df9489208f071f9b7cd70bca025b
Joao Victor <ovictorjocorvinal@gmail.com>
2021-08-19 12:24:56 -0300

    Controle avião

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -1,11 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class Obstaculo : MonoBehaviour
 {
 
-    [SerializeField] private float velocidade = 0.5f;
+    [SerializeField] 
+    private float velocidade = 0.5f;
+    [SerializeField]
+    private float variacaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY, variacaoY));
+    }
+
     void Update()
     {
         this.transform.Translate(Vector3.left * (this.velocidade * Time.deltaTime));
