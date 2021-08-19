feb8e97cf0b67bb5d3693268d8e668cea2df1ec5
Joao Victor <ovictorjocorvinal@gmail.com>
2021-08-19 18:24:30 -0300

    Adicionando interface GameOver

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+   [SerializeField] private GameObject imagemGameOver;
    public void FinalizarJogo()
    {
       Time.timeScale = 0;
+      this.imagemGameOver.SetActive(true);
    }
 }
